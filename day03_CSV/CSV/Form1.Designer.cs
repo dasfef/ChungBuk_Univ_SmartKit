6f372d6fdba0049d6eed0327f274617c3ff6a462
dasfef <96065385+dasfef@users.noreply.github.com>
2023-03-31 12:08:27 +0900

    Add files via upload

---
+++ day03_CSV/CSV/Form1.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace CSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblData = new System.Windows.Forms.Label();
+            this.lblFilePath = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
+            this.lblRate = new System.Windows.Forms.Label();
+            this.cmbPort = new System.Windows.Forms.ComboBox();
+            this.cmbRate = new System.Windows.Forms.ComboBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblTemp = new System.Windows.Forms.Label();
+            this.lblHumi = new System.Windows.Forms.Label();
+            this.lblPpm = new System.Windows.Forms.Label();
+            this.lblData2 = new System.Windows.Forms.Label();
+            this.lblData3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(15, 20);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 36);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblData
+            // 
+            this.lblData.AutoSize = true;
+            this.lblData.Location = new System.Drawing.Point(116, 32);
+            this.lblData.Name = "lblData";
+            this.lblData.Size = new System.Drawing.Size(11, 12);
+            this.lblData.TabIndex = 1;
+            this.lblData.Text = "-";
+            // 
+            // lblFilePath
+            // 
+            this.lblFilePath.AutoSize = true;
+            this.lblFilePath.Location = new System.Drawing.Point(17, 70);
+            this.lblFilePath.Name = "lblFilePath";
+            this.lblFilePath.Size = new System.Drawing.Size(65, 12);
+            this.lblFilePath.TabIndex = 2;
+            this.lblFilePath.Text = "2023-03-31";
+            // 
+            // lblPort
+            // 
+            this.lblPort.AutoSize = true;
+            this.lblPort.Location = new System.Drawing.Point(13, 25);
+            this.lblPort.Name = "lblPort";
+            this.lblPort.Size = new System.Drawing.Size(38, 12);
+            this.lblPort.TabIndex = 3;
+            this.lblPort.Text = "PORT";
+            // 
+            // lblRate
+            // 
+            this.lblRate.AutoSize = true;
+            this.lblRate.Location = new System.Drawing.Point(13, 55);
+            this.lblRate.Name = "lblRate";
+            this.lblRate.Size = new System.Drawing.Size(69, 12);
+            this.lblRate.TabIndex = 4;
+            this.lblRate.Text = "BAUDRATE";
+            // 
+            // cmbPort
+            // 
+            this.cmbPort.FormattingEnabled = true;
+            this.cmbPort.Location = new System.Drawing.Point(104, 21);
+            this.cmbPort.Name = "cmbPort";
+            this.cmbPort.Size = new System.Drawing.Size(121, 20);
+            this.cmbPort.TabIndex = 5;
+            // 
+            // cmbRate
+            // 
+            this.cmbRate.FormattingEnabled = true;
+            this.cmbRate.Location = new System.Drawing.Point(104, 48);
+            this.cmbRate.Name = "cmbRate";
+            this.cmbRate.Size = new System.Drawing.Size(121, 20);
+            this.cmbRate.TabIndex = 6;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(247, 20);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(79, 47);
+            this.btnConnect.TabIndex = 7;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnConnect);
+            this.groupBox1.Controls.Add(this.lblPort);
+            this.groupBox1.Controls.Add(this.cmbRate);
+            this.groupBox1.Controls.Add(this.lblRate);
+            this.groupBox1.Controls.Add(this.cmbPort);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(522, 100);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblData3);
+            this.groupBox2.Controls.Add(this.lblData2);
+            this.groupBox2.Controls.Add(this.btnStart);
+            this.groupBox2.Controls.Add(this.lblData);
+            this.groupBox2.Controls.Add(this.lblFilePath);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(0, 256);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(522, 100);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblPpm);
+            this.groupBox3.Controls.Add(this.lblHumi);
+            this.groupBox3.Controls.Add(this.lblTemp);
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Controls.Add(this.label2);
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(0, 100);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(522, 156);
+            this.groupBox3.TabIndex = 10;
+            this.groupBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TEMP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(214, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "HUMI";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(407, 17);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "PPM";
+            // 
+            // lblTemp
+            // 
+            this.lblTemp.AutoSize = true;
+            this.lblTemp.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTemp.ForeColor = System.Drawing.Color.Red;
+            this.lblTemp.Location = new System.Drawing.Point(37, 84);
+            this.lblTemp.Name = "lblTemp";
+            this.lblTemp.Size = new System.Drawing.Size(68, 45);
+            this.lblTemp.TabIndex = 3;
+            this.lblTemp.Text = "00";
+            // 
+            // lblHumi
+            // 
+            this.lblHumi.AutoSize = true;
+            this.lblHumi.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHumi.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.lblHumi.Location = new System.Drawing.Point(218, 84);
+            this.lblHumi.Name = "lblHumi";
+            this.lblHumi.Size = new System.Drawing.Size(68, 45);
+            this.lblHumi.TabIndex = 4;
+            this.lblHumi.Text = "00";
+            // 
+            // lblPpm
+            // 
+            this.lblPpm.AutoSize = true;
+            this.lblPpm.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPpm.ForeColor = System.Drawing.Color.Gold;
+            this.lblPpm.Location = new System.Drawing.Point(395, 84);
+            this.lblPpm.Name = "lblPpm";
+            this.lblPpm.Size = new System.Drawing.Size(92, 45);
+            this.lblPpm.TabIndex = 5;
+            this.lblPpm.Text = "000";
+            // 
+            // lblData2
+            // 
+            this.lblData2.AutoSize = true;
+            this.lblData2.Location = new System.Drawing.Point(162, 32);
+            this.lblData2.Name = "lblData2";
+            this.lblData2.Size = new System.Drawing.Size(11, 12);
+            this.lblData2.TabIndex = 3;
+            this.lblData2.Text = "-";
+            // 
+            // lblData3
+            // 
+            this.lblData3.AutoSize = true;
+            this.lblData3.Location = new System.Drawing.Point(208, 32);
+            this.lblData3.Name = "lblData3";
+            this.lblData3.Size = new System.Drawing.Size(11, 12);
+            this.lblData3.TabIndex = 4;
+            this.lblData3.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 356);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Sensor Data to CSV";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblData;
+        private System.Windows.Forms.Label lblFilePath;
+        private System.Windows.Forms.Label lblPort;
+        private System.Windows.Forms.Label lblRate;
+        private System.Windows.Forms.ComboBox cmbPort;
+        private System.Windows.Forms.ComboBox cmbRate;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblPpm;
+        private System.Windows.Forms.Label lblHumi;
+        private System.Windows.Forms.Label lblTemp;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblData3;
+        private System.Windows.Forms.Label lblData2;
+    }
+}
+
---
+++ day03_CSV/CSV/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace CSV
+{
+    public partial class Form1 : Form
+    {
+        SerialPort ComPort = new SerialPort();
+        private delegate void SetTextDelegate(string getString);
+
+        public string csvFileName;
+        public StreamWriter csvStream;
+        public string strMessage;
+
+        public int RndValue;
+        public Random rnd = new Random();
+
+        int _temp, _humi, _ppm;
+
+        public Form1()
+        {
+            InitializeComponent();
+            ComPort.DataReceived += new SerialDataReceivedEventHandler(DataReceived);
+        }
+
+        private void DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            string rxd = ComPort.ReadTo("\n");
+            this.BeginInvoke(new SetTextDelegate(SerialReceived), new object[] { rxd });
+        }
+
+        private void SerialReceived(string inString)
+        {
+            string temp = inString.Substring(0, 2);
+            string humi = inString.Substring(3, 2);
+            string ppm = inString.Substring(8);
+
+            lblTemp.Text = temp;
+            lblHumi.Text = humi;
+            lblPpm.Text = ppm;
+
+            _temp = Convert.ToInt16(temp);
+            _humi = Convert.ToInt16(humi);
+            _ppm = Convert.ToInt16(ppm);
+
+            CSV_Write(_temp, _humi, _ppm);
+        }
+
+        public bool CSV_Init()
+        {
+            csvFileName = Environment.CurrentDirectory + "\\" + DateTime.Now.ToString("yyyy-MM-dd") + ".csv";
+            csvStream = File.AppendText(csvFileName);
+            if(File.Exists(csvFileName) == true)
+            {
+                strMessage = DateTime.Now.ToString("HH:mm:ss") + "," + "#" + "," + "On System";
+                csvStream.WriteLine(strMessage);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void CSV_Close()
+        {
+            if(File.Exists(csvFileName) == true)
+            {
+                strMessage = DateTime.Now.ToString("HH:mm:ss") + "," + "#" + "," + "On System";
+                csvStream.WriteLine(strMessage);
+                csvStream.Close();
+            }
+        }
+
+        public void CSV_Start()
+        {
+            if(File.Exists(csvFileName) == true)
+            {
+                strMessage = DateTime.Now.ToString("HH:mm:ss") + "," + "#" + "," + "Start System";
+                csvStream.WriteLine(strMessage);
+                strMessage = "Time, Class, Temp, Humi, PPM";
+                csvStream.WriteLine(strMessage);
+
+            }
+        }
+
+        public void CSV_Pause()
+        {
+            strMessage = DateTime.Now.ToString("HH:mm:ss") + "," + "#" + "," + "Stop System";
+            csvStream.WriteLine(strMessage);
+        }
+
+        public void CSV_Write(int Data1, int Data2, int Data3)
+        {
+            if(File.Exists(csvFileName) == true)
+            {
+                strMessage = DateTime.Now.ToString("HH:mm:ss") + "," + "@" + "," + Data1.ToString() + "," + Data2.ToString() + "," + Data3.ToString();
+                csvStream.WriteLine(strMessage);
+            }
+            else
+            {
+                csvFileName = Environment.CurrentDirectory + "\\" + DateTime.Now.ToString("yyyy-MM-dd") + ".csv";
+                csvStream = File.AppendText(csvFileName);
+                if(File.Exists(csvFileName) == true)
+                {
+                    strMessage = DateTime.Now.ToString("HH:mm:ss") + "," + "#" + "," + "Rework System";
+                    csvStream.WriteLine(strMessage);
+                    strMessage = DateTime.Now.ToString("HH:mm:ss") + "," + "@" + "," + Data1.ToString() + "," + Data2.ToString() + "," + Data3.ToString();
+                    csvStream.WriteLine(strMessage);
+                }
+                lblFilePath.Text = csvFileName;
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var portName = System.IO.Ports.SerialPort.GetPortNames();
+            cmbPort.Items.AddRange(portName);
+            cmbPort.SelectedIndex = cmbPort.Items.Count - 1;
+
+            cmbRate.Items.Clear();
+            cmbRate.Items.Add("9600");
+            cmbRate.Items.Add("19200");
+            cmbRate.Items.Add("57600");
+            cmbRate.Items.Add("115200");
+            cmbRate.SelectedIndex = 0;
+
+            if (CSV_Init())
+            {
+                lblFilePath.Text = csvFileName;
+            }
+            else
+            {
+                lblFilePath.Text = "Failed csv File";
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CSV_Close();
+
+            if (ComPort.IsOpen)
+            {
+                ComPort.Close();
+                ComPort.Dispose();
+                ComPort = null;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if(btnStart.Text == "Start")
+            {
+                CSV_Start();
+                timer1.Start();
+                btnStart.Text = "Stop";
+            }
+            else
+            {
+                timer1.Stop();
+                CSV_Pause();
+                btnStart.Text = "Start";
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //RndValue = rnd.Next(100);
+
+            lblData.Text = _temp.ToString();
+            lblData2.Text = _humi.ToString();
+            lblData3.Text = _ppm.ToString();
+            //lblData.Text = RndValue.ToString();
+            //CSV_Write(RndValue);
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (btnConnect.Text == "Connect")
+            {
+                ComPort.PortName = cmbPort.Text;
+                ComPort.BaudRate = Convert.ToInt32(cmbRate.Text);
+                ComPort.DataBits = 8;
+                ComPort.Parity = Parity.None;
+                ComPort.StopBits = StopBits.One;
+                ComPort.Handshake = Handshake.None;
+                ComPort.Open();
+                ComPort.DiscardInBuffer();
+                btnConnect.Text = "Close";
+            }
+            else
+            {
+                ComPort.Close();
+                btnConnect.Text = "Connect";
+            }
+        }
+    }
+}
